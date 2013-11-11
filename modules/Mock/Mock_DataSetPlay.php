4003ba87b121401c26bd73aaee587a09c4ab3819
Viacheslav Poturaev <nanopeni@gmail.com>
2013-11-11 10:27:49 +0700

    string parser test

--- modules/Mock/Mock_DataSetPlay.php
+++ modules/Mock/Mock_DataSetPlay.php
@@ -3,21 +3,6 @@
 class Mock_DataSetPlay extends Mock_DataSetBase {
     protected $sequenceId = 0;
 
-    public function add($key, $value, $section = null) {
-        if (null === $key) {
-            $key = $this->sequenceId++;
-        }
-
-        if (null !== $key) {
-            $key = array($section, $key);
-        }
-
-        if ($this->branchKey) {
-            $key = array_merge($this->branchKey, is_array($key) ? $key : array($key));
-        }
-
-        $this->storage->set($key, $value);
-    }
 
     public function get($key = null, $section = null) {
         if (null === $key) {
