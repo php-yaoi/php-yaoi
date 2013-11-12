761e0db0535cd1fefcfb59250e7e9ae44a1115d3
Viacheslav Poturaev <nanopeni@gmail.com>
2013-11-12 15:51:30 +0700

    logger

--- modules/Abstract/Abstract_Log.php
+++ modules/Abstract/Abstract_Log.php
@@ -1,12 +1,46 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: poturaev
- * Date: 17.09.13
- * Time: 10:37
- * To change this template use File | Settings | File Templates.
- */
 
-class Abstract_Log {
+class Abstract_Log extends Base_Class
+{
+    private $handle;
+    private $hits = 0;
+    private $filename;
+
+    public static $path = './logs/';
+
+    private static $logs = array();
+
+    /**
+     * @param $filename
+     * @return self
+     */
+    public static function get($filename)
+    {
+        if (!isset(self::$logs[$filename])) {
+            self::$logs[$filename] = new self($filename);
+        }
+        return self::$logs[$filename];
+    }
+
+
+    private function __construct($filename)
+    {
+        $this->filename = '/' == $filename[0] ? $filename : self::$path . $filename;
+    }
+
+
+    public function write($message)
+    {
+        $message = date('Y-m-d H:i:s') . "\t" . $message . "\n";
+
+        if (++$this->hits > 5) {
+            if (is_null($this->handle)) {
+                $this->handle = fopen($this->filename, 'a');
+            }
+            fwrite($this->handle, $message);
+        } else {
+            file_put_contents($this->filename, $message, FILE_APPEND);
+        }
+    }
 
 }
