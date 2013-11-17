c37d3a4d45caca942aab76d206a75226fac32c61
vearutop <w@w.ww>
2013-11-17 23:48:15 +0700

    storage db wrapper

--- modules/Storage/Driver/Storage_Driver_DatabaseWrapper.php
+++ modules/Storage/Driver/Storage_Driver_DatabaseWrapper.php
@@ -1,5 +1,35 @@
 <?php
 
-class Storage_Driver_DatabaseWrapper {
+class Storage_Driver_DatabaseWrapper extends Storage_Driver {
+    /**
+     * @var Database_Client
+     */
+    protected $databaseClient;
+    public function connect() {
+        $this->dsn;
+    }
+
+
+    function get($key)
+    {
+
+        $this->databaseClient->query();
+    }
+
+    function keyExists($key)
+    {
+    }
+
+    function set($key, $value, $ttl)
+    {
+    }
+
+    function delete($key)
+    {
+    }
+
+    function deleteAll()
+    {
+    }
 
 } 
