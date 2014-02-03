a89c666faed3218bcd073b96560885638d98aeb9
Viacheslav Poturaev <nanopeni@gmail.com>
2014-02-03 21:01:44 +0700

    test performance

--- modules/Test/PHPUnit/Test_PHPUnit_Case.php
+++ modules/Test/PHPUnit/Test_PHPUnit_Case.php
@@ -4,7 +4,7 @@ class Test_PHPUnit_Case  extends PHPUnit_Framework_TestCase {
     public static $settings = array();
 
     public function runTest() {
-        echo 'testing ' , str_pad(get_called_class() . '->' . $this->getName() . ',', 30, ' ');
+        echo 'testing ' , str_pad(get_called_class() . '->' . $this->getName() . ',', 50, ' ');
         $s = microtime(1);
         parent::runTest();
         echo "\t", round(1000 * (microtime(1) - $s)), " ms.\n";
