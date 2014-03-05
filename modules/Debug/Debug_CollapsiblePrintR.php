fc71c090ad129cc2bbd82c680d7653b4daae8300
Viacheslav Poturaev <nanopeni@gmail.com>
2014-03-05 23:39:05 +0700

    collapsible print_r

--- modules/Debug/Debug_CollapsiblePrintR.php
+++ modules/Debug/Debug_CollapsiblePrintR.php
@@ -16,18 +16,29 @@ class Debug_CollapsiblePrintR extends View_HTML_Element {
         parent::renderTail();
         ?>
 <script>
-    var phpPrintRBeautifier = {
-        prepareString: function (s) {
+    (function(){
+        var ss=document.getElementsByTagName('script');var c = (ss[ss.length-1]).previousSibling;
+
+        function prepareString(s) {
             return s.replace(/(Array|Object)\n(\s*)\(/g,
-                    '<span class="debug-controls"><a class="toggle-display" href="#" onclick="phpPrintRBeautifier.toggleDisplay(this.parentNode.nextSibling);return false;">$1</a> ' +
-                        '<a class="toggle-children" href="#" onclick="phpPrintRBeautifier.toggleChildren(this.parentNode.nextSibling, false);return false" title="toggle children">\\</a> ' +
-                        '<a class="toggle-recursive" href="#" onclick="phpPrintRBeautifier.toggleChildren(this.parentNode.nextSibling, true);return false;" title="toggle recursive">*</a> ' +
+                    '<span class="debug-controls"><a class="toggle-display" href="#">$1</a> ' +
+                        '<a class="toggle-children" href="#" title="toggle children">\\</a> ' +
+                        '<a class="toggle-recursive" href="#" title="toggle recursive">*</a> ' +
                         '</span><span class="debug-data" style="display:none"> ' +
                         '\n$2(')
                 .replace(/\n(\s*?)\)\n/g, '\n$1)\n</span>');
-        },
+        }
 
-        autoShow: function (e, level) {
+        function toggleDisplay(e, show) {
+            if ('undefined' == typeof show) {
+                show = autoShow(e, 0);
+            }
+
+            e.style.display = show ? '' : 'none';
+            return show;
+        }
+
+        function autoShow(e, level) {
             var show;
             if (!level) {
                 return e.style.display == 'none';
@@ -35,7 +46,7 @@ class Debug_CollapsiblePrintR extends View_HTML_Element {
             else {
                 for (var i = 0; i < e.childNodes.length; ++i) {
                     if ('debug-data' == e.childNodes[i].className) {
-                        show = this.autoShow(e.childNodes[i], level - 1);
+                        show = autoShow(e.childNodes[i], level - 1);
                         if (-1 != show) {
                             return show;
                         }
@@ -43,57 +54,61 @@ class Debug_CollapsiblePrintR extends View_HTML_Element {
                 }
             }
             return -1;
-        },
+        }
 
 
-        toggleDisplay: function (e, show) {
+        function toggleChildren(e, recursive, show) {
             if ('undefined' == typeof show) {
-                show = this.autoShow(e, 0);
-            }
-
-            e.style.display = show ? '' : 'none';
-            return show;
-        },
-
-        toggleChildren: function (e, recursive, show) {
-            if ('undefined' == typeof show) {
-                show = this.autoShow(e, recursive ? 2 : 1);
+                show = autoShow(e, recursive ? 2 : 1);
                 if (-1 == show) {
                     if (recursive) {
-                        return this.toggleChildren(e, false);
+                        toggleChildren(e, false);
+                        return;
                     }
                     else {
-                        return this.toggleDisplay(e);
+                        toggleDisplay(e);
+                        return;
                     }
                 }
             }
             for (var i = 0; i < e.childNodes.length; ++i) {
                 if ('debug-data' == e.childNodes[i].className) {
-                    this.toggleDisplay(e.childNodes[i], show);
+                    toggleDisplay(e.childNodes[i], show);
                     if (recursive) {
-                        this.toggleChildren(e.childNodes[i], true, show);
+                        toggleChildren(e.childNodes[i], true, show);
                     }
                 }
             }
             if (show) {
-                this.toggleDisplay(e, show);
-            }
-        },
-
-        prepare: function (e) {
-            e.innerHTML = this.prepareString(e.innerHTML);
-            var a = e.getElementsByTagName('a');
-            for (var i = 0; i <= a.length; ++i) {
-                var c = a[i].className;
-                if (c == 'toggle-display') {
-                    a[i].click = function(e) {
-                        phpPrintRBeautifier.toggleDisplay(this.parentNode.nextSibling);
-                        return false;
-                    }
-                }
+                toggleDisplay(e, show);
             }
         }
-    };
+
+        function prepare(e) {
+            e.innerHTML = prepareString(e.innerHTML);
+            var a = e.getElementsByTagName('a');
+            for (var i = 0; i < a.length; ++i) {
+                a[i].onclick = function() {
+                    var cls = this.className, element = this.parentNode.nextSibling;
+                    if (cls == 'toggle-display') {
+                        toggleDisplay(element);
+                    }
+                    else if (cls == 'toggle-children') {
+                        toggleChildren(element, false);
+                    }
+                    else if (cls == 'toggle-recursive') {
+                        toggleChildren(element, true);
+                    }
+
+
+                    return false;
+                };
+            }
+        }
+
+        prepare(c);
+
+    })();
 </script>
     <?php
     }
