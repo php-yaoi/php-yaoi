3e277c2ba952958e8585c2a61992695231369f58
Viacheslav Poturaev <nanopeni@gmail.com>
2014-03-10 14:18:47 +0700

    storage log

--- modules/Test/PHPUnit/Test_PHPUnit_Case.php
+++ modules/Test/PHPUnit/Test_PHPUnit_Case.php
@@ -12,4 +12,12 @@ class Test_PHPUnit_Case  extends PHPUnit_Framework_TestCase {
         self::$totalRuntime += $s;
         echo "\t", round(1000 * $s), " ms.,\t tot. ", round(1000 * self::$totalRuntime), " ms.\n";
     }
+
+
+
+    public function assertStringEqualsCRLF($expected, $actual, $message = '') {
+        $expected = str_replace("\r\n", "\n", $expected);
+        $actual = str_replace("\r\n", "\n", $actual);
+        $this->assertSame($expected, $actual, $message);
+    }
 } 
