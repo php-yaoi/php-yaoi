66d34f7797fab46a70ac1ac6aa65a8de9081bf83
Viacheslav Poturaev <nanopeni@gmail.com>
2014-04-14 14:00:16 +0700

    initialize null dsn, Class_Dsn is now required!

--- modules/Database/Abstract/Database_Abstract_Dsn.php
+++
@@ -1,9 +0,0 @@
-<?php
-abstract class Database_Abstract_Dsn extends String_Dsn {
-    public $persistent = false;
-    public $reconnect = false;
-    public $logQueries = false;
-    public $unixSocket;
-    public $charset;
-    public $timezone;
-}
