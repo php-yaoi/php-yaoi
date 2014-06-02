73bb112bb2169548e598f3e9233d789171848c6f
Viacheslav Poturaev <nanopeni@gmail.com>
2014-06-02 13:40:39 +0700

    rows

--- modules/Rows/Rows_SetIterator.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Rows_SetIterator extends RecursiveArrayIterator {
-    public function hasChildren() {
-        return is_array($this->current()) || $this->current() instanceof Iterator;
-    }
-} 
