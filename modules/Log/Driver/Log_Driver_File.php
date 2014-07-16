bbe1ee41f612cb9f038cef389ab75e0a8efcab11
Viacheslav Poturaev <nanopeni@gmail.com>
2014-07-16 15:31:12 +0700

    refactoring

--- modules/Log/Driver/Log_Driver_File.php
+++ modules/Log/Driver/Log_Driver_File.php
@@ -13,7 +13,7 @@ class Log_Driver_File implements Log_Driver {
         $this->fileName = $dsn->path;
         $this->fileName = '/' == $this->fileName[0]
             ? $this->fileName
-            : App::instance()->logPath . $this->fileName;
+            : Acme::instance()->logPath . $this->fileName;
     }
 
     /**
