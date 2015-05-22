a28d87818f2703af504a5475bf2b09a344dc7114
Viacheslav Poturaev <vearutop@gmail.com>
2015-05-22 12:50:11 +0700

    complex sql symbols

--- modules/Sql/Sql_Symbol.php
+++ modules/Sql/Sql_Symbol.php
@@ -2,9 +2,14 @@
 
 
 class Sql_Symbol {
-    // TODO support complex 'table.field' symbols
+    public $names = array();
     public $name;
-    public function __construct($name) {
-        $this->name = $name;
+    public function __construct($name, $name2 = null, $name3 = null) {
+        foreach (func_get_args() as $nameItem) {
+            if ($nameItem) {
+                $this->names []= $nameItem;
+            }
+        }
+        $this->name = $this->names[0];
     }
 } 
