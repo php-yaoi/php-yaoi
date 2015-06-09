8631c4cbcab38909aa13e12936b03386973fbbc7
Viacheslav Poturaev <vearutop@gmail.com>
2015-06-09 17:30:13 +0700

    tests

--- modules/Test/PHPUnit/Test_PHPUnit_Case.php
+++ modules/Test/PHPUnit/Test_PHPUnit_Case.php
@@ -3,14 +3,21 @@
 class Test_PHPUnit_Case  extends PHPUnit_Framework_TestCase {
     public static $settings = array();
     private static $totalRuntime = 0;
+    private $verbose;
+    private $debug;
 
     public function runTest() {
-        echo 'testing ' , str_pad(get_called_class() . '->' . $this->getName() . ',', 50, ' ');
+
+        $this->verbose = in_array('--verbose', $_SERVER['argv'], true) || in_array('-v', $_SERVER['argv'], true);
+        $this->debug = in_array('--debug', $_SERVER['argv'], true);
+
         $s = microtime(1);
         parent::runTest();
         $s = microtime(1) - $s;
         self::$totalRuntime += $s;
-        echo "\t", round(1000 * $s), " ms.,\t tot. ", round(1000 * self::$totalRuntime), " ms.\n";
+        if ($this->debug) {
+            echo "\t", round(1000 * $s), " ms.,\t tot. ", round(1000 * self::$totalRuntime), " ms.\n";
+        }
     }
 
 
