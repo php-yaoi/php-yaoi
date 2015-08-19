42757b3bdfab39e699b36d0e72d549aaae4dce48
Viacheslav Poturaev <nanopeni@gmail.com>
2015-08-19 08:24:05 +0700

    db entity migrations

--- src/Migration/Settings.php
+++ src/Migration/Settings.php
@@ -10,10 +10,4 @@ class Settings extends \Yaoi\Service\Settings
      * @var \Yaoi\Storage|\Yaoi\Storage\Settings|string
      */
     public $storage;
-
-    /**
-     * Closure gets owner Migration_Client as argument
-     * @var \Closure
-     */
-    public $run;
 }
